f8d569d3025c8530a0a10312730bf4c1bb574152
Yanhui Shen <shen.elf@gmail.com>
2019-01-24 13:59:21 +0800

    Init

---
+++ install.sh
@@ -0,0 +1,9 @@
+root=/usr/local
+mkdir -p $root/share/v2ray/
+mkdir -p $root/etc/v2ray/
+cp v2ctl v2ray $root/bin/
+cp geoip.dat geosite.dat $root/share/v2ray
+cp config.json $root/etc/v2ray/
+cp rc $root/etc/rc.d/v2ray
+pw addgroup -n v2ray
+pw adduser v2ray -g v2ray -d /nonexistent -s /usr/sbin/nologin -c "v2ray user"
