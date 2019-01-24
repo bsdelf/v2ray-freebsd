59ea861d05f7edf3fa2bc9d612a751a535050374
Yanhui Shen <shen.elf@gmail.com>
2019-01-24 22:23:47 +0800

    Refactor

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ cp geoip.dat geosite.dat $root/share/v2ray
 cp config.json $root/etc/v2ray/
 cp rc $root/etc/rc.d/v2ray
 pw addgroup -n v2ray
-pw adduser v2ray -g v2ray -d /nonexistent -s /usr/sbin/nologin -c "v2ray user"
+pw adduser v2ray -g v2ray -d /nonexistent -s /usr/sbin/nologin -c "V2ray Daemon"
